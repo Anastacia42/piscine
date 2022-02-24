4a3a3d7622f21a7c64ae176ae3d75eb3917c9e9d
ansilva- <97514783+Anastacia42@users.noreply.github.com>
2022-02-24 16:40:15 +0000

    Delete Shell_01/intra-uuid-dc7e6aa7-ebf6-4eca-aef2-5f3f9d3f44c6-3946312-ansilva- directory

--- Shell_01/intra-uuid-dc7e6aa7-ebf6-4eca-aef2-5f3f9d3f44c6-3946312-ansilva-/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v '^#' | awk 'NR%2==0'| awk -F: '{print $1}' | rev | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | tr '\n' ',' | sed 's/,$/./' | tr -d '\n'
